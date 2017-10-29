f684181ea669f9625af868dd06742543eb17bdcf
Mochammad Taufiq <mochammad.taufiq@dayseven.digital>
2017-10-29 16:10:31 +0700

    improve

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const app = express();
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/webhook', line.middleware(config), (req, res) => {
-
 	Promise
 		.all(req.body.events.map(handleEvent))
 		.then((result) => res.json(result));
@@ -35,31 +34,55 @@ function handleEvent(event) {
 		return Promise.resolve(null);
 	}
 
-	
-	var text = event.message.text;
-	var input = text.split(' ');	
-
-	switch(input[0]) {
+	var text = event.message.text;	
+	switch(text) {
 		case '/surat':
 			getSurah(event.replyToken);
 			break;
+		case '/help':
+			getHelp(event.replyToken);
+			break;
 		default:
-			getAyah(input, event.replyToken)
+			getAyah(text, event.replyToken)
 	}
 }
 
-const LIST_SURAH = ["Al-Faatiha", "Al-Baqara", "Aal-i-Imraan", "An-Nisaa", "Al-Maaida", "Al-An'aam", "Al-A'raaf", "Al-Anfaal", "At-Tawba", "Yunus", "Hud", "Yusuf", "Ar-Ra'd", "Ibrahim", "Al-Hijr", "An-Nahl", "Al-Israa", "Al-Kahf", "Maryam", "Taa-Haa", "Al-Anbiyaa", "Al-Hajj", "Al-Muminoon", "An-Noor", "Al-Furqaan", "Ash-Shu'araa", "An-Naml", "Al-Qasas", "Al-Ankaboot", "Ar-Room", "Luqman", "As-Sajda", "Al-Ahzaab", "Saba", "Faatir", "Yaseen", "As-Saaffaat", "Saad", "Az-Zumar", "Ghafir", "Fussilat", "Ash-Shura", "Az-Zukhruf", "Ad-Dukhaan", "Al-Jaathiya", "Al-Ahqaf", "Muhammad", "Al-Fath", "Al-Hujuraat", "Qaaf", "Adh-Dhaariyat", "At-Tur", "An-Najm", "Al-Qamar", "Ar-Rahmaan", "Al-Waaqia", "Al-Hadid", "Al-Mujaadila", "Al-Hashr", "Al-Mumtahana", "As-Saff", "Al-Jumu'a", "Al-Munaafiqoon", "At-Taghaabun", "At-Talaaq", "At-Tahrim", "Al-Mulk", "Al-Qalam", "Al-Haaqqa", "Al-Ma'aarij", "Nooh", "Al-Jinn", "Al-Muzzammil", "Al-Muddaththir", "Al-Qiyaama", "Al-Insaan", "Al-Mursalaat", "An-Naba", "An-Naazi'aat", "Abasa", "At-Takwir", "Al-Infitaar", "Al-Mutaffifin", "Al-Inshiqaaq", "Al-Burooj", "At-Taariq", "Al-A'laa", "Al-Ghaashiya", "Al-Fajr", "Al-Balad", "Ash-Shams", "Al-Lail", "Ad-Dhuhaa", "Ash-Sharh", "At-Tin", "Al-Alaq", "Al-Qadr", "Al-Bayyina", "Az-Zalzala", "Al-Aadiyaat", "Al-Qaari'a", "At-Takaathur", "Al-Asr", "Al-Humaza", "Al-Fil", "Quraish", "Al-Maa'un", "Al-Kawthar", "Al-Kaafiroon", "An-Nasr", "Al-Masad", "Al-Ikhlaas", "Al-Falaq", "An-Naas"]
+function getHelp(token) {
+	var msg = "ketik /surat untuk melihat daftar surat.\n";
+		msg += "ketik namaSurat:noAyat untuk melihat isi ayat dan terjemahan";
 
-function getSurah(token) {
-	var surahs = LIST_SURAH.join('\n');
 
-	const echo = { type: 'text', text: surahs };
+	const echo = { type: 'text', text: msg };
 
 	client.replyMessage(token, echo);
+
+}
+
+function getSurah(token) {
+	axios.get('http://api.alquran.cloud/surah')
+	.then(response => {
+		const text = [];
+		response.data.data.forEach(function(item, i){
+			const surahNo = item.number;
+			const surahName = item.name;
+			const surahEnglishName = item.englishName;
+
+			text.push(surahNo + '. ' + surahEnglishName);
+		});
+
+		const msg = text.join('\n');
+		const echo = { type: 'text', text: msg };
+
+		client.replyMessage(token, echo);
+	})
+	.catch(error => {
+		console.log(error);
+	});
 }
 
 
-function getAyah(input, token) {
+function getAyah(text, token) {
+	var input = text.split(':');
 	var noSurah = input[0];
 	var noAyah = input[1];
 
@@ -69,18 +92,24 @@ function getAyah(input, token) {
 		var surahId = noSurah;
 	}
 
-	axios.get('http://api.alquran.cloud/surah/'+ surahId +'/editions/id.indonesian')
+	axios.get('http://api.alquran.cloud/surah/'+ surahId +'/editions/quran-uthmani,id.indonesian')
 	.then(response => {
-		response.data.data.forEach(function(item){
+		var msg = [];
+		response.data.data.forEach(function(item, i){
 			var surah = item.englishName;
 			var ayah = item.ayahs[noAyah].text;
-			var text = ayah + ' (QS. ' + surah + ': ' + noAyah + ')';
 
-			const echo = { type: 'text', text: text };
+			if(i ==  0) {
+				var text = ayah;
+			}else{
+				var text = ayah + ' (QS. ' + surah + ': ' + noAyah + ')';	
+			}
 
-			console.log(echo)
-			client.replyMessage(token, echo);
+			msg.push({ type: 'text', text: text });
 		});
+
+		console.log(msg)
+		client.replyMessage(token, msg);
 	})
 	.catch(error => {
 		console.log(error);
